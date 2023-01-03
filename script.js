441c997b5bce996e60e7167659bd86d011f3eddb
libao <leo900527@gmail.com>
2023-01-03 23:13:26 +0800

    Button Hover

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function make_left_page(){
     let myDiv = document.getElementsByClassName('left-page')[0]
-    console.log(myDiv)
+    
+    // Build Button Panel
     let buttonDiv = document.createElement('div')
     buttonDiv.name = 'button-panel'
     buttonDiv.style.height = '7%'
@@ -15,7 +16,12 @@ function make_left_page(){
     button.innerHTML = 'Temperature';
     button.className = 'DisplayButton rightButton'
     buttonDiv.appendChild(button)
+
+
+    // Build Chart
+    
     
 }
+
 
 make_left_page()
